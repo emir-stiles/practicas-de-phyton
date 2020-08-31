0c05581aeec799cf2cc03b417ffd9a97feec0b01
Emir Stiles <70492089+emir-stiles@users.noreply.github.com>
2020-08-30 21:30:26 -0500

    ejercicio 1 entrada de caracteres

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,8 +1,10 @@
 
 name = input("escribe tu nombre  ")
 age = input("escribe tu edad  ")
+veces = int(input("escribe un numero del 1 al 10 "))
 nage = int (age)
 print("tu nombre es  " + name)
 print("tu edad es  "+ age)
 new_age= str(nage + 25)
-print("tendrá " + new_age + " en el 2045" )
+message = (" tendrá " + new_age + " en el 2045 " )
+print(veces * message)
