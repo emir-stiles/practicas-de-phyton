6cade2e7b8096e0f19983f9d20205e4214242333
Emir Stiles <70492089+emir-stiles@users.noreply.github.com>
2020-08-30 19:50:12 -0500

    actividad 1 character imput

---
+++ PythonApplication2.py
@@ -0,0 +1,8 @@
+
+name = input("escribe tu nombre  ")
+age = input("escribe tu edad  ")
+nage = int (age)
+print("tu nombre es  " + name)
+print("tu edad es  "+ age)
+new_age= str(nage + 25)
+print("tendrá " + new_age + " en el 2045" )
